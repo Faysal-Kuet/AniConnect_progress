ad1970e85195688d21a3b64738b3ab3df3e816b0
Faysal Mahmud <pi.faysal.mahmud@gmail.com>
2024-11-21 02:59:17 +0600

    Initial Commit

---
+++ AniConnect/AniConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniConnectApp.swift
+//  AniConnect
+//
+//  Created by Faysal Mahmud on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AniConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AniConnect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AniConnect
+//
+//  Created by Faysal Mahmud on 21/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
